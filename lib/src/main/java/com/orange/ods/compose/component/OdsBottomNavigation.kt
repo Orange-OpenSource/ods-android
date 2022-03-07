2818699fae2dd189e298038c3a0be4ee89de4ac2
Pauline Auvray <pauline.auvray@orange.com>
2022-03-07 14:52:27 +0100

    Add ODS custom Composables for bottom navigation

---
+++ lib/src/main/java/com/orange/ods/compose/component/OdsBottomNavigation.kt
@@ -0,0 +1,59 @@
+/*
+ *
+ *  Copyright 2021 Orange
+ *
+ *  Use of this source code is governed by an MIT-style
+ *  license that can be found in the LICENSE file or at
+ *  https://opensource.org/licenses/MIT.
+ * /
+ */
+
+package com.orange.ods.compose.component
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@Composable
+fun OdsBottomNavigation(
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit
+) {
+    androidx.compose.material.BottomNavigation(
+        modifier = modifier,
+        // Need to define backgroundColor cause in Compose default backgroundColor is primarySurface
+        backgroundColor = MaterialTheme.colors.surface,
+        content = content
+    )
+}
+
+@Composable
+fun RowScope.OdsBottomNavigationItem(
+    selected: Boolean,
+    onClick: () -> Unit,
+    icon: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    label: @Composable (() -> Unit)? = null,
+    alwaysShowLabel: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+) {
+    BottomNavigationItem(
+        selected = selected,
+        onClick = onClick,
+        icon = icon,
+        modifier = modifier,
+        enabled = enabled,
+        label = label,
+        alwaysShowLabel = alwaysShowLabel,
+        interactionSource = interactionSource,
+        // Need to define these attributes cause they are not taken from style
+        selectedContentColor = MaterialTheme.colors.primary,
+        unselectedContentColor = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
+    )
+}
