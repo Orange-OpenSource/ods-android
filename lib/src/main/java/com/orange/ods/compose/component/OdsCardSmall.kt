b7b6daed2639d075dfb2ecded8edc023c7240bd7
RemiCholleyOcto <65963161+RemiCholleyOcto@users.noreply.github.com>
2022-03-16 16:35:53 +0100

    Create component OdsCardSmall

---
+++ lib/src/main/java/com/orange/ods/compose/component/OdsCardSmall.kt
@@ -0,0 +1,75 @@
+/*
+ *
+ *  Copyright 2021 Orange
+ *
+ *  Use of this source code is governed by an MIT-style
+ *  license that can be found in the LICENSE file or at
+ *  https://opensource.org/licenses/MIT.
+ * /
+ */
+
+package com.orange.ods.compose.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
+import com.orange.ods.R
+
+@Composable
+fun OdsCardSmall(
+    modifier: Modifier = Modifier,
+    title: String,
+    subtitle: String? = null,
+    @DrawableRes
+    imageRes: Int,
+    imageContentDescription: String? = null,
+    onCardClick: (() -> Unit)? = null,
+) {
+    Card(
+        modifier = modifier.clickable {
+            onCardClick?.invoke()
+        }
+    ) {
+        Column {
+            Image(
+                painter = painterResource(imageRes),
+                contentDescription = imageContentDescription,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(dimensionResource(R.dimen.ods_card_small_image_width)),
+
+                )
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .semantics(mergeDescendants = true) {}
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.subtitle1
+                )
+                subtitle?.let {
+                    Text(
+                        text = it,
+                        style = MaterialTheme.typography.subtitle2
+                    )
+                }
+            }
+        }
+    }
+}
