a82df2a72b6f68b8ac2e2506a42add8c8d17978b
Pauline Auvray <pauline.auvray@orange.com>
2022-04-06 09:05:33 +0200

    Create common composables used in the demo app

---
+++ demo/src/main/java/com/orange/ods/demo/ui/utils/Composables.kt
@@ -0,0 +1,55 @@
+/*
+ *
+ *  Copyright 2021 Orange
+ *
+ *  Use of this source code is governed by an MIT-style
+ *  license that can be found in the LICENSE file or at
+ *  https://opensource.org/licenses/MIT.
+ * /
+ */
+
+package com.orange.ods.demo.ui.utils
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import com.orange.ods.compose.text.OdsTextH5
+import com.orange.ods.compose.text.OdsTextSubtitle1
+import com.orange.ods.compose.theme.OdsDisplayAppearance
+import com.orange.ods.compose.theme.odsDarkThemeColors
+import com.orange.ods.compose.theme.odsLightThemeColors
+import com.orange.ods.demo.R
+
+@Composable
+fun Title(@StringRes textRes: Int) {
+    OdsTextH5(
+        textRes = textRes,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = dimensionResource(R.dimen.ods_spacing_l))
+            .padding(horizontal = dimensionResource(R.dimen.ods_screen_horizontal_margin))
+    )
+}
+
+@Composable
+fun Subtitle(@StringRes textRes: Int, displayAppearance: OdsDisplayAppearance = OdsDisplayAppearance.DEFAULT) {
+    val backgroundColor = when (displayAppearance) {
+        OdsDisplayAppearance.DEFAULT -> Color.Unspecified
+        OdsDisplayAppearance.ON_DARK -> odsDarkThemeColors.background
+        OdsDisplayAppearance.ON_LIGHT -> odsLightThemeColors.background
+    }
+    OdsTextSubtitle1(
+        textRes = textRes,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(backgroundColor)
+            .padding(top = dimensionResource(id = R.dimen.ods_spacing_s))
+            .padding(horizontal = dimensionResource(R.dimen.ods_screen_horizontal_margin)),
+        displayAppearance = displayAppearance
+    )
+}
