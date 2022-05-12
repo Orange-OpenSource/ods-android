071cf25f711bda969746a276cd3fb2c37983cd75
Pauline Auvray <pauline.auvray@orange.com>
2022-05-12 14:04:59 +0200

    Display dialog according customization form

--- demo/src/main/java/com/orange/ods/demo/ui/components/dialogs/ComponentDialogs.kt
+++ demo/src/main/java/com/orange/ods/demo/ui/components/dialogs/ComponentDialogs.kt
@@ -10,44 +10,69 @@
 
 package com.orange.ods.demo.ui.components.dialogs
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.orange.ods.compose.component.button.OdsButton
+import com.orange.ods.compose.component.control.OdsCheckbox
+import com.orange.ods.compose.component.dialog.OdsAlertDialog
+import com.orange.ods.compose.component.list.OdsListItem
 import com.orange.ods.demo.R
 import com.orange.ods.demo.ui.components.buttons.fullWidthButton
-import com.orange.ods.demo.ui.utilities.composable.LabelledCheckbox
 import com.orange.ods.demo.ui.utilities.composable.Title
 
+@ExperimentalMaterialApi
 @Composable
 fun ComponentDialogsContent() {
     val customizationState = rememberDialogCustomizationState()
+    val openDialog = rememberSaveable { mutableStateOf(false) }
+    val closeDialogAction = { openDialog.value = false }
+    val confirmActionRes = if (customizationState.isDismissButtonChecked) R.string.component_dialog_action_confirm else R.string.component_dialog_action_ok
 
     Title(textRes = R.string.component_dialogs_customize, withHorizontalPadding = true)
-    LabelledCheckbox(
-        checked = customizationState.titleChecked,
-        label = stringResource(id = R.string.component_element_title)
+    Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.ods_spacing_xs)))
+    DialogCustomizationListItem(R.string.component_element_title, customizationState.titleChecked)
+    DialogCustomizationListItem(R.string.component_dialog_element_dismiss_button, customizationState.dismissButtonChecked)
+    OdsButton(
+        modifier = Modifier.fullWidthButton(),
+        text = stringResource(id = R.string.component_dialogs_open_dialog),
+        hasPrimaryColor = true,
+        onClick = {
+            openDialog.value = true
+        }
     )
-    LabelledCheckbox(
-        checked = customizationState.iconChecked,
-        label = stringResource(id = R.string.component_element_icon)
-    )
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Start
-    ) {
-        LabelledCheckbox(
-            checked = customizationState.button1Checked,
-            label = stringResource(id = R.string.component_element_button1)
-        )
-        LabelledCheckbox(
-            checked = customizationState.button2Checked,
-            label = stringResource(id = R.string.component_element_button2)
+
+    if (openDialog.value) {
+        OdsAlertDialog(
+            titleText = if (customizationState.isTitleChecked) {
+                stringResource(id = R.string.component_element_title)
+            } else null,
+            text = stringResource(id = R.string.component_dialog_text),
+            confirmButtonText = stringResource(id = confirmActionRes),
+            onConfirmButtonClick = closeDialogAction,
+            dismissButtonText = if (customizationState.isDismissButtonChecked) {
+                stringResource(id = R.string.component_dialog_action_dismiss)
+            } else null,
+            onDismissButtonClick = closeDialogAction
         )
     }
+}
 
-    OdsButton(modifier = Modifier.fullWidthButton(), text = stringResource(id = R.string.component_dialogs_open_dialog), hasPrimaryColor = true, onClick = { /*TODO*/ })
+@ExperimentalMaterialApi
+@Composable
+private fun DialogCustomizationListItem(@StringRes labelRes: Int, checked: MutableState<Boolean>) {
+    OdsListItem(
+        modifier = Modifier.clickable { checked.value = !checked.value },
+        text = stringResource(id = labelRes),
+        trailing = { OdsCheckbox(checked = checked.value, onCheckedChange = { checked.value = it }) }
+    )
 }
--- demo/src/main/java/com/orange/ods/demo/ui/components/dialogs/DialogCustomizationState.kt
+++ demo/src/main/java/com/orange/ods/demo/ui/components/dialogs/DialogCustomizationState.kt
@@ -19,18 +19,19 @@ import androidx.compose.runtime.saveable.rememberSaveable
 @Composable
 fun rememberDialogCustomizationState(
     titleChecked: MutableState<Boolean> = rememberSaveable { mutableStateOf(false) },
-    iconChecked: MutableState<Boolean> = rememberSaveable { mutableStateOf(false) },
-    button1Checked: MutableState<Boolean> = rememberSaveable { mutableStateOf(false) },
-    button2Checked: MutableState<Boolean> = rememberSaveable { mutableStateOf(false) },
+    dismissButtonChecked: MutableState<Boolean> = rememberSaveable { mutableStateOf(false) },
 ) =
-    remember(titleChecked, iconChecked, button1Checked, button2Checked) {
-        DialogCustomizationState(titleChecked, iconChecked, button1Checked, button2Checked)
+    remember(titleChecked, dismissButtonChecked) {
+        DialogCustomizationState(titleChecked, dismissButtonChecked)
     }
 
 class DialogCustomizationState(
     val titleChecked: MutableState<Boolean>,
-    val iconChecked: MutableState<Boolean>,
-    val button1Checked: MutableState<Boolean>,
-    val button2Checked: MutableState<Boolean>
+    val dismissButtonChecked: MutableState<Boolean>,
 ) {
+    val isTitleChecked
+        get() = titleChecked.value
+
+    val isDismissButtonChecked
+        get() = dismissButtonChecked.value
 }
