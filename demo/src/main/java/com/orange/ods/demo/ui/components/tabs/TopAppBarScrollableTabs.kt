4e6f86b24b3f65418cca6581661e51f439dc6096
Pauline Auvray <pauline.auvray@orange.com>
2022-05-31 15:29:26 +0200

    [#108] Remove useless imports

--- demo/src/main/java/com/orange/ods/demo/ui/components/tabs/TopAppBarScrollableTabs.kt
+++ demo/src/main/java/com/orange/ods/demo/ui/components/tabs/TopAppBarScrollableTabs.kt
@@ -12,15 +12,9 @@ package com.orange.ods.demo.ui.components.tabs
 
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.PagerState
-import com.orange.ods.compose.component.tab.OdsLeadingIconTab
 import com.orange.ods.compose.component.tab.OdsScrollableTabRow
-import com.orange.ods.compose.component.tab.OdsTab
-import kotlinx.coroutines.launch
 
 @ExperimentalMaterialApi
 @ExperimentalPagerApi
