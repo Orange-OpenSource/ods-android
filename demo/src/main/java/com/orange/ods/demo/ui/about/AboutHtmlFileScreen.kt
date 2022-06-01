6c16cc3f8101f8348240ff47830a16a21cfa1575
Florent Maitre <florent.maitre@orange.com>
2022-06-01 15:58:46 +0200

    [#131] Fix minor UI bug with padding in legal notice and privacy policy

--- demo/src/main/java/com/orange/ods/demo/ui/about/AboutHtmlFileScreen.kt
+++ demo/src/main/java/com/orange/ods/demo/ui/about/AboutHtmlFileScreen.kt
@@ -10,15 +10,14 @@
 
 package com.orange.ods.demo.ui.about
 
+import android.annotation.SuppressLint
 import android.content.res.Configuration.UI_MODE_NIGHT_MASK
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.graphics.Color
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
@@ -40,14 +39,19 @@ fun AboutHtmlFileScreen(
         updateTopBarTitle(item.titleRes)
         val context = LocalContext.current
         val configuration = LocalConfiguration.current
+        val horizontalPadding = dimensionResource(id = R.dimen.ods_screen_horizontal_margin).value
+        val verticalPadding = dimensionResource(id = R.dimen.ods_screen_vertical_margin).value
         AndroidView(
-            modifier = Modifier.padding(
-                horizontal = dimensionResource(id = R.dimen.ods_screen_horizontal_margin),
-                vertical = dimensionResource(id = R.dimen.ods_screen_vertical_margin)
-            ),
             factory = {
                 WebView(context).apply {
-                    webViewClient = WebViewClient()
+                    @SuppressLint("SetJavaScriptEnabled")
+                    settings.javaScriptEnabled = true
+                    webViewClient = object : WebViewClient() {
+                        override fun onPageFinished(view: WebView?, url: String?) {
+                            super.onPageFinished(view, url)
+                            view?.loadUrl("javascript:(function(){ document.body.style.padding = '${verticalPadding}px ${horizontalPadding}px' })();");
+                        }
+                    }
                     loadUrl("${FILE_PATH}${item.fileName}")
                     setBackgroundColor(Color.TRANSPARENT)
                     if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK_STRATEGY)) {
