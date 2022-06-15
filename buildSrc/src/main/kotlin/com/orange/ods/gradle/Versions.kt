e8caa15dbb8674090d4364875987141e5e92e667
Pauline Auvray <pauline.auvray@orange.com>
2022-06-15 09:44:55 +0200

    [#113] Upgrade compose version to take benefit of Chip composable available in the new version

--- buildSrc/src/main/kotlin/com/orange/ods/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/com/orange/ods/gradle/Versions.kt
@@ -12,7 +12,7 @@ package com.orange.ods.gradle
 
 object Versions {
 
-    const val compileSdk = 31
+    const val compileSdk = 32
     const val minSdk = 21
     const val targetSdk = compileSdk
 
@@ -20,14 +20,15 @@ object Versions {
     const val activity = "1.4.0"
     const val androidGradlePlugin = "7.2.0"
     const val appCompat = "1.4.1"
-    const val compose = "1.1.1"
+    const val compose = "1.2.0-beta03"
+    const val composeUiTooling = "1.1.1"
     const val core = "1.7.0"
     const val googleServicesGradlePlugin = "4.3.10"
     const val firebaseAppDistributionGradlePlugin = "3.0.1"
     const val firebaseBom = "30.0.0"
     const val firebaseCrashlyticsGradlePlugin = "2.8.1"
     const val jUnit = "4.13.2"
-    const val kotlin = "1.6.10"
+    const val kotlin = "1.6.21"
     const val lifecycle = "2.4.1"
     const val material = "1.5.0"
     const val navigation = "2.4.1"
