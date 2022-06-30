b89787749cb7cbb7bbe78119e8741e02fc8d602a
Pauline Auvray <pauline.auvray@orange.com>
2022-06-30 09:47:09 +0200

    [#113] Review: Reorder dependencies

--- buildSrc/src/main/kotlin/com/orange/ods/gradle/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/orange/ods/gradle/Dependencies.kt
@@ -12,10 +12,10 @@ package com.orange.ods.gradle
 
 object Dependencies {
 
-    const val accompanistSystemUiController = "com.google.accompanist:accompanist-systemuicontroller:${Versions.accompanist}"
+    const val accompanistFlowLayout = "com.google.accompanist:accompanist-flowlayout:${Versions.accompanist}"
     const val accompanistPager = "com.google.accompanist:accompanist-pager:${Versions.accompanist}"
     const val accompanistPagerIndicators = "com.google.accompanist:accompanist-pager-indicators:${Versions.accompanist}"
-    const val accompanistFlowLayout = "com.google.accompanist:accompanist-flowlayout:${Versions.accompanist}"
+    const val accompanistSystemUiController = "com.google.accompanist:accompanist-systemuicontroller:${Versions.accompanist}"
     const val activityCompose = "androidx.activity:activity-compose:${Versions.activity}"
     const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.androidGradlePlugin}"
     const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
