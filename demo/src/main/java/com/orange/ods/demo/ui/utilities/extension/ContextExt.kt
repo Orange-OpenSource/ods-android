b03940f1f478ec89608edf82cba9a381c7439cba
Florent Maitre <florent.maitre@orange.com>
2022-06-30 18:51:39 +0200

    [#181] Links in about screens are now open using custom tabs

--- demo/src/main/java/com/orange/ods/demo/ui/utilities/extension/ContextExt.kt
+++ demo/src/main/java/com/orange/ods/demo/ui/utilities/extension/ContextExt.kt
@@ -10,8 +10,21 @@
 
 package com.orange.ods.demo.ui.utilities
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.net.Uri
+import androidx.browser.customtabs.CustomTabsIntent
 
 //Method to get the resource name with the color id
 fun Context.getStringName(res: Int): String =
     this.resources.getResourceName(res).split('/').last()
+
+fun Context.launchUrl(url: String) {
+    try {
+        CustomTabsIntent.Builder()
+            .setUrlBarHidingEnabled(true)
+            .build()
+            .launchUrl(this, Uri.parse(url))
+    } catch (e: ActivityNotFoundException) {
+    }
+}
