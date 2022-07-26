05530001dc24f80cac0b985bee5e690cd30b23bc
Florent Maitre <florent.maitre@orange.com>
2022-07-26 13:38:35 +0200

    [#172] Use ComponentCountRow in bottom navigation demo

--- demo/src/main/java/com/orange/ods/demo/ui/components/bottomnavigation/ComponentBottomNavigation.kt
+++ demo/src/main/java/com/orange/ods/demo/ui/components/bottomnavigation/ComponentBottomNavigation.kt
@@ -11,7 +11,6 @@
 package com.orange.ods.demo.ui.components.bottomnavigation
 
 import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -20,7 +19,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -33,24 +31,19 @@ import com.orange.ods.compose.component.bottomnavigation.OdsBottomNavigation
 import com.orange.ods.compose.component.bottomnavigation.OdsBottomNavigationItem
 import com.orange.ods.demo.R
 import com.orange.ods.demo.ui.components.Component
+import com.orange.ods.demo.ui.components.utilities.ComponentCountRow
 import com.orange.ods.demo.ui.components.utilities.ComponentHeader
 import com.orange.ods.demo.ui.components.utilities.clickOnElement
-import com.orange.ods.demo.ui.utilities.composable.LabelledRadioButton
 
-private data class BottomNavigationVariant(@StringRes val titleRes: Int, val itemNumber: Int)
-
-private val bottomNavigationVariants = listOf(
-    BottomNavigationVariant(R.string.component_bottom_navigation_three_up, 3),
-    BottomNavigationVariant(R.string.component_bottom_navigation_four_up, 4),
-    BottomNavigationVariant(R.string.component_bottom_navigation_five_up, 5)
-)
+private const val minNavigationItemCount = 3
+private const val maxNavigationItemCount = 5
 
 @Composable
 fun ComponentBottomNavigation() {
-    val selectedVariantIndex = rememberSaveable { mutableStateOf(0) }
+    val selectedNavigationItemCount = rememberSaveable { mutableStateOf(minNavigationItemCount) }
 
     Scaffold(
-        bottomBar = { BottomNavigationBar(selectedVariantIndex) }
+        bottomBar = { BottomNavigationBar(selectedNavigationItemCount.value) }
     ) { innerPadding ->
         Box(modifier = Modifier.padding(innerPadding)) {
             Column(
@@ -64,14 +57,13 @@ fun ComponentBottomNavigation() {
                 Column(
                     modifier = Modifier.padding(top = dimensionResource(id = R.dimen.spacing_m))
                 ) {
-                    bottomNavigationVariants.forEachIndexed { index, bottomNavigationVariant ->
-                        LabelledRadioButton(
-                            selectedRadio = mutableStateOf(bottomNavigationVariants[selectedVariantIndex.value].titleRes.toString()),
-                            currentRadio = bottomNavigationVariant.titleRes.toString(),
-                            label = stringResource(id = bottomNavigationVariant.titleRes),
-                            onClick = { selectedVariantIndex.value = index }
-                        )
-                    }
+                    ComponentCountRow(
+                        modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.ods_screen_horizontal_margin)),
+                        title = stringResource(id = R.string.component_bottom_navigation_navigation_item_count),
+                        count = selectedNavigationItemCount,
+                        minCount = minNavigationItemCount,
+                        maxCount = maxNavigationItemCount
+                    )
                 }
             }
         }
@@ -79,7 +71,7 @@ fun ComponentBottomNavigation() {
 }
 
 @Composable
-private fun BottomNavigationBar(selectedVariantIndex: MutableState<Int>) {
+private fun BottomNavigationBar(navigationItemCount: Int) {
     val context = LocalContext.current
     val navigationItems = listOf(
         NavigationItem("Favorites", R.drawable.ic_heart),
@@ -89,17 +81,17 @@ private fun BottomNavigationBar(selectedVariantIndex: MutableState<Int>) {
         NavigationItem("Settings", R.drawable.ic_settings)
     )
 
-    val selectedItem = remember { mutableStateOf(navigationItems[0]) }
+    val selectedNavigationItem = remember { mutableStateOf(navigationItems[0]) }
 
     OdsBottomNavigation {
-        for (item in navigationItems.subList(0, bottomNavigationVariants[selectedVariantIndex.value].itemNumber)) {
+        navigationItems.take(navigationItemCount).forEach { navigationItem ->
             OdsBottomNavigationItem(
-                icon = { Icon(painter = painterResource(id = item.icon), contentDescription = null) },
-                label = item.title,
-                selected = selectedItem.value.title == item.title,
+                icon = { Icon(painter = painterResource(id = navigationItem.icon), contentDescription = null) },
+                label = navigationItem.title,
+                selected = selectedNavigationItem.value.title == navigationItem.title,
                 onClick = {
-                    selectedItem.value = item
-                    clickOnElement(context, item.title)
+                    selectedNavigationItem.value = navigationItem
+                    clickOnElement(context, navigationItem.title)
                 }
             )
         }
