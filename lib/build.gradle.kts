05579b1cfee9437f14cb613849e1da08ef6a35d8
Florent Maitre <florent.maitre@orange.com>
2022-10-17 15:07:45 +0200

    [#298] Fix a bug where Gradle failed to resolve component-processor dependency when using released ods-lib

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -65,7 +65,7 @@ android {
 
 dependencies {
     implementation(Dependencies.kotlinStdlibJdk8)
-    implementation(project(":component-processor"))
+    compileOnly(project(":component-processor"))
 
     api(Dependencies.material)
 
