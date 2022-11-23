63d64bb86368b9decaae3dc3ce850bcf6baf0617
Pauline Auvray <pauline.auvray@orange.com>
2022-11-23 16:16:19 +0100

    [#114] Review: Use a composable to display the snackbar demo screen like the dialog demo screen

--- demo/src/main/java/com/orange/ods/demo/ui/components/snackbars/ComponentSnackbars.kt
+++ demo/src/main/java/com/orange/ods/demo/ui/components/snackbars/ComponentSnackbars.kt
@@ -10,24 +10,18 @@
 
 package com.orange.ods.demo.ui.components.snackbars
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import com.orange.ods.compose.component.button.OdsOutlinedButton
 import com.orange.ods.compose.component.snackbar.OdsSnackbar
 import com.orange.ods.compose.component.snackbar.OdsSnackbarHost
 import com.orange.ods.demo.R
 import com.orange.ods.demo.ui.components.utilities.ComponentCustomizationBottomSheetScaffold
+import com.orange.ods.demo.ui.components.utilities.ComponentLaunchContentColumn
 import com.orange.ods.demo.ui.utilities.composable.SwitchListItem
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -52,20 +46,13 @@ fun ComponentSnackbars() {
         bottomSheetContent = {
             SwitchListItem(labelRes = R.string.component_snackbar_action_button, checked = actionButtonChecked)
         }) {
-        Column(modifier = Modifier.fillMaxWidth()) {
-            OdsOutlinedButton(
-                modifier = Modifier
-                    .align(alignment = Alignment.CenterHorizontally)
-                    .padding(top = dimensionResource(id = R.dimen.screen_vertical_margin)),
-                text = stringResource(id = R.string.component_snackbar_show),
-                onClick = {
-                    coroutineScope.launch {
-                        scaffoldState.snackbarHostState.showSnackbar(
-                            message = snackbarMessage,
-                            actionLabel = if (actionButtonChecked.value) snackbarActionLabel else null
-                        )
-                    }
-                })
+        ComponentLaunchContentColumn(textRes = R.string.component_snackbar_customize, buttonLabelRes = R.string.component_snackbar_show) {
+            coroutineScope.launch {
+                scaffoldState.snackbarHostState.showSnackbar(
+                    message = snackbarMessage,
+                    actionLabel = if (actionButtonChecked.value) snackbarActionLabel else null
+                )
+            }
         }
     }
 }
