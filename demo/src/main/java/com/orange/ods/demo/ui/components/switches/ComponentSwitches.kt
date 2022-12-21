119c2b9ea26ee5e69b7661ff692eb3f2c53c5c90
Florent Maitre <florent.maitre@orange.com>
2022-12-21 18:33:13 +0100

    [#388] Replace content of switches component demo with recipes

--- demo/src/main/java/com/orange/ods/demo/ui/components/switches/ComponentSwitches.kt
+++ demo/src/main/java/com/orange/ods/demo/ui/components/switches/ComponentSwitches.kt
@@ -17,7 +17,9 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -25,6 +27,7 @@ import androidx.compose.ui.res.stringResource
 import com.orange.ods.compose.component.list.OdsListItem
 import com.orange.ods.compose.component.list.OdsSwitchTrailing
 import com.orange.ods.demo.R
+import com.orange.ods.demo.domain.recipes.LocalRecipes
 import com.orange.ods.demo.ui.components.utilities.ComponentCustomizationBottomSheetScaffold
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -32,6 +35,8 @@ import com.orange.ods.demo.ui.components.utilities.ComponentCustomizationBottomS
 fun ComponentSwitches() {
     val checked = rememberSaveable { mutableStateOf(false) }
     val enabled = rememberSaveable { mutableStateOf(true) }
+    val recipes = LocalRecipes.current
+    val recipe by remember { mutableStateOf(recipes.random()) }
 
     ComponentCustomizationBottomSheetScaffold(
         bottomSheetScaffoldState = rememberBottomSheetScaffoldState(),
@@ -49,7 +54,7 @@ fun ComponentSwitches() {
                 .padding(bottom = dimensionResource(id = R.dimen.spacing_m))
         ) {
             OdsListItem(
-                text = stringResource(id = R.string.component_element_label),
+                text = recipe.title,
                 trailing = OdsSwitchTrailing(
                     checked = checked,
                     enabled = enabled.value
