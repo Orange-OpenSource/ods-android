377c1831140bd48551fadfafa42111641c0135fb
Pauline Auvray <pauline.auvray@orange.com>
2023-01-06 15:03:01 +0100

    [#391] Review: Set semantics merge descendants to true for non clickable cards to fix vocalization of small card

--- lib/src/main/java/com/orange/ods/compose/component/card/OdsCard.kt
+++ lib/src/main/java/com/orange/ods/compose/component/card/OdsCard.kt
@@ -14,8 +14,6 @@ import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
 import com.orange.ods.compose.theme.OdsTheme
 
@@ -27,13 +25,11 @@ internal fun OdsCard(modifier: Modifier, onClick: (() -> Unit)?, content: @Compo
 
     if (onClick != null) {
         Card(
-            modifier = modifier.semantics(mergeDescendants = true) {
-                role = Role.Button
-            }, backgroundColor = backgroundColor, contentColor = contentColor, onClick = onClick, content = content
+            modifier = modifier, backgroundColor = backgroundColor, contentColor = contentColor, onClick = onClick, content = content
         )
     } else {
         Card(
-            modifier = modifier, backgroundColor = backgroundColor, contentColor = contentColor, content = content
+            modifier = modifier.semantics(mergeDescendants = true) {}, backgroundColor = backgroundColor, contentColor = contentColor, content = content
         )
     }
 }
