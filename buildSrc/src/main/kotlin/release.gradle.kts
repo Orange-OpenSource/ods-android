17dd80d0c345ea47ace5802f8a8beadfe849c32d
Florent Maitre <florent.maitre@orange.com>
2023-01-27 18:26:07 +0100

    [#258] Add documentation versioning

--- buildSrc/src/main/kotlin/release.gradle.kts
+++ buildSrc/src/main/kotlin/release.gradle.kts
@@ -14,31 +14,15 @@ import java.util.*
 
 tasks.register<DefaultTask>("prepareRelease") {
     doLast {
-        val version = project.gradle.startParameter.projectProperties["version"]
-        if (version == null) {
+        val newVersion = project.gradle.startParameter.projectProperties["version"]
+        if (newVersion == null) {
             throw GradleException("Please set the \"version\" project property.")
         }
 
-        // Get previous version from gradle.properties
-        val previousVersion = execute("sed", "-En", "s/^version=(.*)/\\1/p", "gradle.properties").trim()
-
-        // Update version in gradle.properties
-        execute("sed", "-Ei", "", "s/^(version=).*/\\1$version/", "gradle.properties")
-
-        // Update dependencies in docs/home_content.md and DEVELOP.md
-        val substituteDependencyArg = "s/(com\\.orange\\.ods\\.android:ods-[^:]*:)[[:digit:]]+\\.[[:digit:]]+\\.[[:digit:]]+/\\1$version/"
-        execute("sed", "-Ei", "", substituteDependencyArg, "docs/home_content.md")
-        execute("sed", "-Ei", "", substituteDependencyArg, "DEVELOP.md")
-
-        // Update unreleased section title in changelog.md
-        val date = SimpleDateFormat("yyyy-MM-dd").format(Date())
-        execute(
-            "sed",
-            "-Ei",
-            "",
-            "s/^## \\[Unreleased\\].*/## [$version](https:\\/\\/github.com\\/Orange-OpenSource\\/ods-android\\/compare\\/$previousVersion...$version) - $date/",
-            "changelog.md"
-        )
+        updateVersion(newVersion)
+        updateDependencies(newVersion)
+        updateChangelog(newVersion)
+        archiveDocumentation(newVersion)
     }
 }
 
@@ -48,4 +32,50 @@ tasks.register<DefaultTask>("tagRelease") {
         execute("git", "tag", tag)
         execute("git", "push", "origin", tag)
     }
+}
+
+fun updateVersion(version: String) {
+    execute("sed", "-Ei", "", "s/^(version=).*/\\1$version/", "gradle.properties")
+}
+
+fun updateDependencies(version: String) {
+    val substituteDependencyArg = "s/(com\\.orange\\.ods\\.android:ods-[^:]*:)[[:digit:]]+\\.[[:digit:]]+\\.[[:digit:]]+/\\1$version/"
+    execute("sed", "-Ei", "", substituteDependencyArg, "docs/home_content.md")
+    execute("sed", "-Ei", "", substituteDependencyArg, "DEVELOP.md")
+}
+
+fun updateChangelog(version: String) {
+    val previousVersion = execute(
+        "sed",
+        "-En",
+        "s/^## \\[([[:digit:]]+\\.[[:digit:]]+\\.[[:digit:]])\\].*/\\1/p",
+        "changelog.md"
+    ).substringBefore("\n")
+    val date = SimpleDateFormat("yyyy-MM-dd").format(Date())
+    execute(
+        "sed",
+        "-Ei",
+        "",
+        "s/^## \\[Unreleased\\].*/## [$version](https:\\/\\/github.com\\/Orange-OpenSource\\/ods-android\\/compare\\/$previousVersion...$version) - $date/",
+        "changelog.md"
+    )
+}
+
+fun archiveDocumentation(version: String) {
+    copy {
+        from("docs")
+        into("docs/$version")
+        exclude("_*", "Gemfile*")
+    }
+
+    val jekyllConfigFile = File("docs/_config.yml")
+    jekyllConfigFile.appendText(
+        """
+            |  - scope:
+            |      path: "$version"
+            |    values:
+            |      version: "$version"
+            |
+            """.trimMargin()
+    )
 }
