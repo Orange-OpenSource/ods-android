8846d1ef4863fa8ab80b51299cec5f53ac62a7ba
Pauline Auvray <pauline.auvray@orange.com>
2023-03-23 11:26:19 +0100

    [#118] Initialize Timber

--- app/src/main/java/com/orange/ods/app/OdsApplication.kt
+++ app/src/main/java/com/orange/ods/app/OdsApplication.kt
@@ -14,16 +14,25 @@ import android.app.Application
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class OdsApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initializeLogger()
         initializeCrashlytics()
     }
 
     private fun initializeCrashlytics() {
         Firebase.crashlytics.setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
     }
+
+    private fun initializeLogger() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
 }
