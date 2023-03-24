1af0cb483095271dfbaaa1305db00173643bff59
Doline Touko <doline.touko@orange.com>
2023-03-24 15:19:44 +0100

    [#478] search with component and guideline

--- app/src/main/java/com/orange/ods/app/ui/search/SearchScreen.kt
+++ app/src/main/java/com/orange/ods/app/ui/search/SearchScreen.kt
@@ -19,16 +19,16 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
+import com.orange.ods.app.R
 import com.orange.ods.app.ui.LocalOdsGuideline
 import com.orange.ods.app.ui.components.components
 import com.orange.ods.app.ui.guidelines.color.DialogColor
 import com.orange.ods.app.ui.guidelines.color.getValue
-import com.orange.ods.app.ui.guidelines.spacing.GuidelineSpacingImage
-import com.orange.ods.app.ui.guidelines.spacing.Spacing
 import com.orange.ods.compose.component.list.OdsListItem
 import com.orange.ods.compose.component.list.OdsListItemIcon
 import com.orange.ods.compose.component.list.OdsListItemIconType
@@ -44,60 +44,72 @@ fun SearchScreen(searchedText: MutableState<TextFieldValue>, onComponentClick: (
     val filterGuidelines = guidelineColors.filter { color ->
         searchedText.value.text.isEmpty() || color.getName().lowercase().contains(searchedText.value.text.lowercase()) ||
                 color.lightThemeName.lowercase().contains(searchedText.value.text.lowercase()) ||
-                color.darkThemeName.lowercase().contains(searchedText.value.text.lowercase()
-        )
+                color.darkThemeName.lowercase().contains(searchedText.value.text.lowercase())
     }
+    
+    data class OdsSearchParameter(
+        val title: String,
+        val route: Long,
+        val image: Int?,
+        val color: Color?
+    )
 
+    //
+    val searchList: List<OdsSearchParameter> = filterComponents.map {
+        val componentImageRes = it.smallImageRes.orElse { it.imageRes }
+        OdsSearchParameter(stringResource(id = it.titleRes), it.id, componentImageRes, color = null)
+    }.plus(filterGuidelines.map {
+        OdsSearchParameter(it.getName(), 0, image = null, color = it.getValue())
+    }).sortedBy { it.title }
 
     LazyColumn(
         modifier = Modifier
             .fillMaxWidth()
     ) {
-        items(filterComponents) { component ->
-            val componentImageRes = component.smallImageRes.orElse { component.imageRes }
-            OdsListItem(
-                text = stringResource(id = component.titleRes),
-                secondaryText = null,
-                singleLineSecondaryText = false,
-                modifier = Modifier
-                    .iconType(OdsListItemIconType.SquareImage)
-                    .clickable { onComponentClick(component.id) },
-                icon = {
-                    OdsListItemIcon(
-                        painterResource(id = componentImageRes)
-                    )
-                },
-            )
-        }
-        items(filterGuidelines) { guideline ->
+        items(searchList) { item ->
             val openDialog = remember { mutableStateOf(false) }
+            var guideline = filterGuidelines.filter {
+                it.getName() == item.title && it.getValue() == item.color
+            }
             OdsListItem(
-                text = guideline.getName(),
+                text = item.title,
                 secondaryText = null,
                 singleLineSecondaryText = false,
                 modifier = Modifier
                     .iconType(OdsListItemIconType.SquareImage)
-                    .clickable { openDialog.value = true },
+                    .clickable {
+                        if (item.image != null) {
+                            onComponentClick(item.route)
+                        } else if (item.color != null) {
+                            openDialog.value = true
+                        }
+                    },
                 icon = {
                     OdsListItemIcon(
-                        painter = ColorPainter(guideline.getValue())
+                        painter = if (item.image != null) {
+                            painterResource(id = item.image)
+                        } else if (item.color != null) {
+                            ColorPainter(item.color)
+                        } else {
+                            painterResource(id = R.drawable.placeholder)
+                        }
                     )
-                },
+                }
             )
             if (openDialog.value) {
-                DialogColor(color = guideline, openDialog)
+                DialogColor(color = guideline[0], openDialog)
             }
         }
-        items(Spacing.values()) { spacing ->
+        /*items(Spacing.values()) { spacing ->
+            val dp = spacing.getDp()
             OdsListItem(
                 text = spacing.tokenName,
-                secondaryText = null,
+                secondaryText = stringResource(id = R.string.guideline_spacing_dp, dp.value.toInt()) + "\n",
                 singleLineSecondaryText = false,
                 modifier = Modifier
-                    .iconType(OdsListItemIconType.SquareImage)
                     .clickable { },
                 icon = { GuidelineSpacingImage(spacing = spacing) },
             )
-        }
+        }*/
     }
 }
