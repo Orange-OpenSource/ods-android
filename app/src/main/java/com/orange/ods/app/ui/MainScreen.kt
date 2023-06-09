fb318327b7f55081ca5f00c7dc13a7ac6adda418
Pauline Auvray <pauline.auvray@orange.com>
2023-06-09 14:38:16 +0200

    Remove titleRes parameter from MainTopAppBar composable cause we can get it by using provided state

--- app/src/main/java/com/orange/ods/app/ui/MainScreen.kt
+++ app/src/main/java/com/orange/ods/app/ui/MainScreen.kt
@@ -107,7 +107,6 @@ fun MainScreen(themeConfigurations: Set<OdsThemeConfigurationContract>, mainView
                         Column {
                             SystemBarsColorSideEffect()
                             MainTopAppBar(
-                                titleRes = mainState.topAppBarState.titleRes.value,
                                 shouldShowUpNavigationIcon = !mainState.shouldShowBottomBar,
                                 state = mainState.topAppBarState,
                                 upPress = mainState::upPress,
