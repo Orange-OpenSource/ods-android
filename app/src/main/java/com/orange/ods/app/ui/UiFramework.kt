b468a84fabc006670efc3324b7224625971a61ed
Florent Maitre <florent.maitre@orange.com>
2023-06-29 16:20:38 +0200

    [#561] Fix a bug where selected UI framework in dropdown menu did not correspond to code implementation

--- app/src/main/java/com/orange/ods/app/ui/UiFramework.kt
+++ app/src/main/java/com/orange/ods/app/ui/UiFramework.kt
@@ -11,12 +11,8 @@
 package com.orange.ods.app.ui
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.databinding.ViewDataBinding
 import com.orange.ods.app.R
@@ -31,17 +27,8 @@ enum class UiFramework(val iconResId: Int, val labelResId: Int) {
 
 @Composable
 inline fun <reified T : ViewDataBinding> UiFramework(compose: @Composable () -> Unit, noinline xml: T.() -> Unit) {
-    val uiFramework = LocalUiFramework.current
-    // Reset current UI framework to Compose when displaying the content
-    // shouldResetUiFramework is used to avoid calling LaunchedEffect on configuration changes (for instance on device rotation)
-    var shouldResetUiFramework by rememberSaveable { mutableStateOf(true) }
-    if (shouldResetUiFramework) {
-        LaunchedEffect(Unit) {
-            shouldResetUiFramework = false
-            uiFramework.value = UiFramework.Compose
-        }
-    }
-    when (uiFramework.value) {
+    val uiFramework by LocalUiFramework.current
+    when (uiFramework) {
         UiFramework.Compose -> compose()
         UiFramework.Xml -> ViewDataBinding(bind = xml)
     }
