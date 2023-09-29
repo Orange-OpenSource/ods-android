e34f8d27581c15ddb8d9f2a3434c9074b3e86007
Pauline Auvray <pauline.auvray@orange.com>
2023-09-29 10:31:06 +0200

    [#579] Review: Add a NavHostController subclass to manage navigation in the application

--- app/src/main/java/com/orange/ods/app/ui/MainScreen.kt
+++ app/src/main/java/com/orange/ods/app/ui/MainScreen.kt
@@ -156,18 +156,6 @@ fun MainScreen(themeConfigurations: Set<OdsThemeConfigurationContract>, mainView
                         upPress = mainState::upPress
                     )
                 }
-
-                if (changeThemeDialogVisible) {
-                    ChangeThemeDialog(
-                        themeManager = mainState.themeState,
-                        dismissDialog = {
-                            changeThemeDialogVisible = false
-                        },
-                        onThemeSelected = {
-                            mainViewModel.storeUserThemeName(mainState.themeState.currentThemeConfiguration.name)
-                        }
-                    )
-                }
             }
         }
     }
