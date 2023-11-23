2b151d3244986cad5af6f671babe86e5b43f7c31
Pauline Auvray <pauline.auvray@orange.com>
2023-11-23 15:57:10 +0100

    [#19] Add optional menu items customization

--- module-about/src/main/java/com/orange/ods/module/about/configuration/OdsAboutMenuItem.kt
+++ module-about/src/main/java/com/orange/ods/module/about/configuration/OdsAboutMenuItem.kt
@@ -10,7 +10,9 @@
 
 package com.orange.ods.module.about.configuration
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.RawRes
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
@@ -21,25 +23,53 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import com.orange.ods.module.about.R
 
+private enum class AboutMenuItem(@DrawableRes private val iconRes: Int, @StringRes private val textRes: Int, private val position: Int) {
+    PrivacyPolicy(R.drawable.ic_dataprotection, R.string.ods_about_menu_privacy_policy, 100),
+    TermsOfService(R.drawable.ic_tasklist, R.string.ods_about_menu_terms_of_service, 101),
+    AppNews(R.drawable.ic_calendareventinfo, R.string.ods_about_menu_app_news, 102),
+    LegalInformation(R.drawable.ic_legal, R.string.ods_about_menu_legal_information, 103),
+    RateTheApp(R.drawable.ic_review, R.string.ods_about_menu_rate_the_app, 104);
+
+    @Composable
+    fun linkedToFile(@RawRes fileRes: Int) = OdsAbout.FileMenuItem(
+        painterResource(id = iconRes),
+        stringResource(id = textRes),
+        position,
+        fileRes,
+        OdsAbout.FileMenuItem.FileFormat.Html
+    )
+
+    @Composable
+    fun linkedToUrl(url: String) = OdsAbout.UrlMenuItem(
+        painterResource(id = iconRes),
+        stringResource(id = textRes),
+        position,
+        url
+    )
+}
+
+
 @Composable
 @Stable
 internal fun mandatoryMenuItems(@RawRes privacyPolicyFileRes: Int, @RawRes termsOfServiceFileRes: Int): List<OdsAboutMenuItem> = listOf(
-    OdsAboutFileMenuItem(
-        painterResource(id = R.drawable.ic_dataprotection),
-        stringResource(id = R.string.ods_about_menu_privacy_policy),
-        100,
-        privacyPolicyFileRes,
-        OdsAboutFileMenuItem.FileFormat.Html
-    ),
-    OdsAboutFileMenuItem(
-        painterResource(id = R.drawable.ic_tasklist),
-        stringResource(id = R.string.ods_about_menu_terms_of_service),
-        101,
-        termsOfServiceFileRes,
-        OdsAboutFileMenuItem.FileFormat.Html
-    )
+    AboutMenuItem.PrivacyPolicy.linkedToFile(privacyPolicyFileRes),
+    AboutMenuItem.TermsOfService.linkedToFile(fileRes = termsOfServiceFileRes)
 )
 
+@Composable
+internal fun optionalMenuItems(@RawRes appNewsFileRes: Int?, @RawRes legalInformationFileRes: Int?, rateTheAppUrl: String?): List<OdsAbout.MenuItem> {
+    return mutableListOf<OdsAboutMenuItem>().apply {
+        appNewsFileRes?.let {
+            add(AboutMenuItem.AppNews.linkedToFile(fileRes = appNewsFileRes))
+        }
+        legalInformationFileRes?.let {
+            add(AboutMenuItem.LegalInformation.linkedToFile(fileRes = legalInformationFileRes))
+        }
+        rateTheAppUrl?.let {
+            add(AboutMenuItem.RateTheApp.linkedToUrl(url = rateTheAppUrl))
+        }
+    }.toList()
+}
 
 /**
  * Item to display in the about menu
