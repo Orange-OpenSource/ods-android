4202d91b99916d3789b4daa2929884d0fe523b2b
Pauline Auvray <pauline.auvray@orange.com>
2023-11-30 12:10:16 +0100

    [#19] Review: Initialize Timber and re-throw CancellationException

--- module-about/src/main/java/com/orange/ods/module/about/data/appnews/AppNewsRepositoryImpl.kt
+++ module-about/src/main/java/com/orange/ods/module/about/data/appnews/AppNewsRepositoryImpl.kt
@@ -15,6 +15,7 @@ import androidx.annotation.RawRes
 import com.orange.ods.extension.contentAsString
 import com.orange.ods.module.about.domain.appnews.AppNews
 import com.orange.ods.module.about.domain.appnews.AppNewsRepository
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import timber.log.Timber
@@ -29,8 +30,12 @@ internal class AppNewsRepositoryImpl @Inject constructor(private val context: Co
             val jsonString = context.resources.openRawResource(fileResId).contentAsString().orEmpty()
             val appNews = appNewsParser.parseAppNews(jsonString)
             emit(appNews)
-        } catch (_: Exception) {
-            Timber.d("Fail to parse JSON file")
+        } catch (e: Exception) {
+            if (e is CancellationException) {
+                throw e
+            } else {
+                Timber.d("Fail to parse JSON file: ${e.message}")
+            }
         }
     }
 }
