197908704b3452e14aa0a7383d6f978a2050f6d8
Pauline Auvray <pauline.auvray@orange.com>
2023-12-04 11:01:10 +0100

    [#723] Show a selected line below the selected navigation item

--- lib/src/main/java/com/orange/ods/compose/component/bottomnavigation/OdsBottomNavigation.kt
+++ lib/src/main/java/com/orange/ods/compose/component/bottomnavigation/OdsBottomNavigation.kt
@@ -10,6 +10,13 @@
 
 package com.orange.ods.compose.component.bottomnavigation
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
@@ -23,14 +30,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import com.orange.ods.R
 import com.orange.ods.compose.component.OdsComposable
 import com.orange.ods.compose.component.content.OdsComponentIcon
 import com.orange.ods.compose.component.content.OdsComponentScopeContent
 import com.orange.ods.compose.component.utilities.Preview
 import com.orange.ods.compose.component.utilities.UiModePreviews
 import com.orange.ods.compose.theme.OdsTheme
+
+private const val BottomNavigationItemMaxCount = 5
 
 /**
  * <a href="https://system.design.orange.com/0c1af118d/p/042eb8-bottom-navigation/b/30078d" target="_blank">ODS Bottom navigation</a>.
@@ -57,7 +70,35 @@ fun OdsBottomNavigation(
         // Need to define backgroundColor cause in Compose default backgroundColor is primarySurface
         backgroundColor = OdsTheme.colors.component.bottomNavigation.barBackground,
         contentColor = OdsTheme.colors.component.bottomNavigation.barContent,
-        content = { items.forEach { with(it) { Content() } } }
+        content = {
+            items.take(BottomNavigationItemMaxCount).forEach { item ->
+                ConstraintLayout(modifier = Modifier.weight(1f)) {
+                    val (itemRef, lineRef) = createRefs()
+                    val itemPadding = dimensionResource(id = R.dimen.spacing_s)
+                    val selectedLineHeight = dimensionResource(id = R.dimen.spacing_xs)
+                    with(item) {
+                        this@BottomNavigation.Content(modifier = Modifier.constrainAs(itemRef) {
+                            centerTo(parent)
+                        })
+                    }
+                    this@BottomNavigation.AnimatedVisibility(
+                        modifier = Modifier
+                            .constrainAs(lineRef) {
+                                bottom.linkTo(parent.bottom)
+                                start.linkTo(parent.start, itemPadding)
+                                end.linkTo(parent.end, itemPadding)
+                                height = Dimension.value(selectedLineHeight)
+                                width = Dimension.fillToConstraints
+                            },
+                        visible = item.selected,
+                        enter = fadeIn() + slideInVertically { it },
+                        exit = fadeOut() + slideOutVertically { it }
+                    ) {
+                        Box(modifier = Modifier.background(OdsTheme.colors.component.bottomNavigation.itemSelected))
+                    }
+                }
+            }
+        }
     )
 }
 
@@ -100,6 +141,7 @@ object OdsBottomNavigation {
         @Composable
         override fun RowScope.Content(modifier: Modifier) {
             BottomNavigationItem(
+                modifier = modifier,
                 selected = selected,
                 onClick = onClick,
                 icon = { icon.Content() },
@@ -160,7 +202,7 @@ private fun PreviewOdsBottomNavigation() = Preview {
         android.R.drawable.ic_dialog_email to "First item",
         android.R.drawable.ic_dialog_map to "Second item",
         android.R.drawable.ic_dialog_dialer to "Third item",
-        android.R.drawable.ic_dialog_info to "Fourth item"
+        android.R.drawable.ic_dialog_info to "About"
     )
 
     var selectedItemIndex by remember { mutableStateOf(0) }
