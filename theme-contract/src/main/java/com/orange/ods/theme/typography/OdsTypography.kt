75dd3b0a2fc9b59720b4ae99373e11d30091c8da
Pauline Auvray <pauline.auvray@orange.com>
2024-01-09 10:46:47 +0100

    [#615] Review: Remove constructor

--- theme-contract/src/main/java/com/orange/ods/theme/typography/OdsTypography.kt
+++ theme-contract/src/main/java/com/orange/ods/theme/typography/OdsTypography.kt
@@ -15,93 +15,93 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-class OdsTypography internal constructor(
-    val headlineL: TextStyle,
-    val headlineS: TextStyle,
-    val titleL: TextStyle,
-    val titleM: TextStyle,
-    val titleS: TextStyle,
-    val bodyL: TextStyle,
-    val bodyM: TextStyle,
-    val bodyS: TextStyle,
-    val labelL: TextStyle,
-    val labelS: TextStyle
-) {
-    constructor(
-        defaultFontFamily: FontFamily = FontFamily.Default,
-        headlineL: TextStyle = TextStyle(
-            fontWeight = FontWeight.Bold,
-            fontSize = 34.sp,
-            lineHeight = 36.sp,
-            letterSpacing = 0.25.sp
-        ),
-        headlineS: TextStyle = TextStyle(
-            fontWeight = FontWeight.Bold,
-            fontSize = 24.sp,
-            lineHeight = 24.sp,
-            letterSpacing = 0.sp
-        ),
-        titleL: TextStyle = TextStyle(
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
-            lineHeight = 24.sp,
-            letterSpacing = 0.15.sp
-        ),
-        titleM: TextStyle = TextStyle(
-            fontWeight = FontWeight.Bold,
-            fontSize = 16.sp,
-            lineHeight = 24.sp,
-            letterSpacing = 0.15.sp
-        ),
-        titleS: TextStyle = TextStyle(
-            fontWeight = FontWeight.Bold,
-            fontSize = 14.sp,
-            lineHeight = 24.sp,
-            letterSpacing = 0.1.sp
-        ),
-        bodyL: TextStyle = TextStyle(
-            fontWeight = FontWeight.Normal,
-            fontSize = 16.sp,
-            lineHeight = 20.sp,
-            letterSpacing = 0.5.sp
-        ),
-        bodyM: TextStyle = TextStyle(
-            fontWeight = FontWeight.Normal,
-            fontSize = 14.sp,
-            lineHeight = 20.sp,
-            letterSpacing = 0.25.sp
-        ),
-        bodyS: TextStyle = TextStyle(
-            fontWeight = FontWeight.Normal,
-            fontSize = 12.sp,
-            lineHeight = 16.sp,
-            letterSpacing = 0.4.sp
-        ),
-        labelL: TextStyle = TextStyle(
-            fontWeight = FontWeight.Bold,
-            fontSize = 14.sp,
-            lineHeight = 16.sp,
-            letterSpacing = 1.25.sp
-        ),
-        labelS: TextStyle = TextStyle(
-            fontWeight = FontWeight.Bold,
-            fontSize = 10.sp,
-            lineHeight = 16.sp,
-            letterSpacing = 1.5.sp
-        )
-    ) : this(
-        headlineL = headlineL.withDefaultFontFamily(defaultFontFamily),
-        headlineS = headlineS.withDefaultFontFamily(defaultFontFamily),
-        titleL = titleL.withDefaultFontFamily(defaultFontFamily),
-        titleM = titleM.withDefaultFontFamily(defaultFontFamily),
-        titleS = titleS.withDefaultFontFamily(defaultFontFamily),
-        bodyL = bodyL.withDefaultFontFamily(defaultFontFamily),
-        bodyM = bodyM.withDefaultFontFamily(defaultFontFamily),
-        bodyS = bodyS.withDefaultFontFamily(defaultFontFamily),
-        labelL = labelL.withDefaultFontFamily(defaultFontFamily),
-        labelS = labelS.withDefaultFontFamily(defaultFontFamily)
+class OdsTypography(
+    defaultFontFamily: FontFamily = FontFamily.Default,
+    headlineL: TextStyle = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 34.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.25.sp
+    ),
+    headlineS: TextStyle = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+    titleL: TextStyle = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    titleM: TextStyle = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    titleS: TextStyle = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.1.sp
+    ),
+    bodyL: TextStyle = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyM: TextStyle = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    bodyS: TextStyle = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp
+    ),
+    labelL: TextStyle = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 1.25.sp,
+    ),
+    labelS: TextStyle = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 10.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 1.5.sp
     )
+) {
 
+    val headlineL: TextStyle
+    val headlineS: TextStyle
+    val titleL: TextStyle
+    val titleM: TextStyle
+    val titleS: TextStyle
+    val bodyL: TextStyle
+    val bodyM: TextStyle
+    val bodyS: TextStyle
+    val labelL: TextStyle
+    val labelS: TextStyle
+
+    init {
+        this.headlineL = headlineL.withDefaultFontFamily(defaultFontFamily)
+        this.headlineS = headlineS.withDefaultFontFamily(defaultFontFamily)
+        this.titleL = titleL.withDefaultFontFamily(defaultFontFamily)
+        this.titleM = titleM.withDefaultFontFamily(defaultFontFamily)
+        this.titleS = titleS.withDefaultFontFamily(defaultFontFamily)
+        this.bodyL = bodyL.withDefaultFontFamily(defaultFontFamily)
+        this.bodyM = bodyM.withDefaultFontFamily(defaultFontFamily)
+        this.bodyS = bodyS.withDefaultFontFamily(defaultFontFamily)
+        this.labelL = labelL.withDefaultFontFamily(defaultFontFamily)
+        this.labelS = labelS.withDefaultFontFamily(defaultFontFamily)
+    }
 }
 
 private fun TextStyle.withDefaultFontFamily(default: FontFamily): TextStyle {
