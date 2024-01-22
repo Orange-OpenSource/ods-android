86b7ec67dc51b73deab96bd4a4e4a046241fa027
Pauline Auvray <pauline.auvray@orange.com>
2024-01-22 15:02:43 +0100

    [#540] Display OdsTextButton XML code implementation

--- lib-xml/src/main/java/com/orange/ods/xml/utilities/extension/OdsTextButtonStyleExt.kt
+++ lib-xml/src/main/java/com/orange/ods/xml/utilities/extension/OdsTextButtonStyleExt.kt
@@ -19,3 +19,12 @@ import com.orange.ods.compose.component.button.OdsTextButton
  * BE CAREFUL: If the enum values change you have to update associated XML attributes in the lib-xml
  */
 fun OdsTextButton.Style.Companion.fromXmlAttrValue(xmlId: Int): OdsTextButton.Style = OdsTextButton.Style.entries[xmlId]
+
+/**
+ * @return the XML enum value corresponding to this [OdsTextButton.Style]
+ * BE CAREFUL: As there is no way to access XML enum names directly, if an enum name change, you have to update this method.
+ */
+fun OdsTextButton.Style.getXmlEnumValue() = when (this) {
+    OdsTextButton.Style.Default -> "standard"
+    OdsTextButton.Style.Primary -> "primary"
+}
