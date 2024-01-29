808ff6938827dd79e5e6a4cfa85b736e00c03ab8
Florent Maitre <florent.maitre@orange.com>
2024-01-29 11:26:28 +0100

    [#783] Review: Add KDoc to OdsText method

--- lib/src/main/java/com/orange/ods/compose/text/OdsText.kt
+++ lib/src/main/java/com/orange/ods/compose/text/OdsText.kt
@@ -26,6 +26,32 @@ import com.orange.ods.compose.theme.OdsTheme
 import com.orange.ods.compose.utilities.extension.enable
 import com.orange.ods.theme.typography.OdsTextStyle
 
+/**
+ * High level element that displays text and provides semantics / accessibility information.
+ *
+ * @param text The text to be displayed.
+ * @param style ODS style configuration for the text such as color, font, line height etc.
+ * @param modifier [Modifier] to apply to this layout node.
+ * @param displaySurface [OdsDisplaySurface] applied to the text. It allows to force the text display on light or dark surface.
+ * By default, the appearance applied is based on the system night mode value.
+ * @param enabled Modifies the text color so that the content looks enabled or disabled.
+ * @param textAlign The alignment of the text within the lines of the paragraph.
+ * See [TextStyle.textAlign].
+ * @param overflow How visual overflow should be handled.
+ * @param softWrap Whether the text should break at soft line breaks. If false, the glyphs in the
+ * text will be positioned as if there was unlimited horizontal space. If [softWrap] is false,
+ * [overflow] and TextAlign may have unexpected effects.
+ * @param maxLines An optional maximum number of lines for the text to span, wrapping if
+ * necessary. If the text exceeds the given number of lines, it will be truncated according to
+ * [overflow] and [softWrap]. It is required that 1 <= [minLines] <= [maxLines].
+ * @param minLines The minimum height in terms of minimum number of visible lines. It is required
+ * that 1 <= [minLines] <= [maxLines].
+ * @param onTextLayout Callback that is executed when a new text layout is calculated. A
+ * [TextLayoutResult] object that callback provides contains paragraph information, size of the
+ * text, baselines and other details. The callback can be used to add additional decoration or
+ * functionality to the text. For example, to draw selection around the text.
+ */
+
 @Composable
 fun OdsText(
     text: String,
