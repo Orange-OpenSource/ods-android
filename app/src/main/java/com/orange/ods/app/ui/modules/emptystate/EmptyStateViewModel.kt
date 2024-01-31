5e54abff201d6d16bd64045a1927cf6c27303a3d
Pauline Auvray <pauline.auvray@orange.com>
2024-01-31 17:09:17 +0100

    [#119] Review: Update copyright

--- app/src/main/java/com/orange/ods/app/ui/modules/emptystate/EmptyStateViewModel.kt
+++ app/src/main/java/com/orange/ods/app/ui/modules/emptystate/EmptyStateViewModel.kt
@@ -1,11 +1,13 @@
 /*
+ * Software Name: Orange Design System
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
  *
- *  Copyright 2021 Orange
+ * This software is distributed under the MIT licence,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
  *
- *  Use of this source code is governed by an MIT-style
- *  license that can be found in the LICENSE file or at
- *  https://opensource.org/licenses/MIT.
- * /
+ * Software description: Android library of reusable graphical components
  */
 
 package com.orange.ods.app.ui.modules.emptystate
@@ -15,7 +17,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
-class EmptyStateViewModel: ViewModel() {
+class EmptyStateViewModel : ViewModel() {
     var text by mutableStateOf(false)
     var button by mutableStateOf(false)
     var usage by mutableStateOf(EmptyStateUsage.FirstUse)
