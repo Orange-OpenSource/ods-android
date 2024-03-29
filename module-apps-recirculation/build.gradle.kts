4939ffd68d81128ef900d54a8917497d549fc6b6
Pauline Auvray <pauline.auvray@orange.com>
2024-03-29 10:07:16 +0100

    Create module-apps-recirculation

---
+++ module-apps-recirculation/build.gradle.kts
@@ -0,0 +1,42 @@
+/*
+ * Software Name: Orange Design System
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * the text of which is available at https://opensource.org/license/MIT/
+ * or see the "LICENSE" file for more details.
+ *
+ * Software description: Android library of reusable graphical components
+ */
+import com.orange.ods.gradle.Dependencies
+import com.orange.ods.gradle.Versions
+
+plugins {
+    id("library")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-kapt") // This must be the last statement in the plugins {} to avoid "options not recognized" warning
+}
+
+android {
+    namespace = "com.orange.ods.module.appsrecirculation"
+
+    buildFeatures {
+        compose = true
+        viewBinding = true
+        dataBinding = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.composeCompiler
+    }
+}
+
+dependencies {
+    implementation(project(":lib"))
+    implementation(Dependencies.appCompat)
+    implementation(Dependencies.composeMaterial)
+    implementation(Dependencies.hiltAndroid)
+    kapt(Dependencies.hiltCompiler)
+    implementation(Dependencies.navigationCompose)
+    implementation(Dependencies.timber)
+}
