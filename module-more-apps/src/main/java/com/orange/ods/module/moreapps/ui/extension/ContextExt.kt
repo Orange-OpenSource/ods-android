6d9077522e6c7bb204c5e007647f6cdccc3ceafa
Pauline Auvray <pauline.auvray@orange.com>
2024-06-05 16:56:02 +0200

    Review: Replace println by Timber

--- module-more-apps/src/main/java/com/orange/ods/module/moreapps/ui/extension/ContextExt.kt
+++ module-more-apps/src/main/java/com/orange/ods/module/moreapps/ui/extension/ContextExt.kt
@@ -16,6 +16,7 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
+import timber.log.Timber
 
 //TODO Duplicate code with ContextExt.kt from module-about. Need to create an independent module to factorize extensions or keep it duplicate? :/
 internal fun Context.launchUrl(url: String) {
@@ -25,6 +26,6 @@ internal fun Context.launchUrl(url: String) {
             .build()
             .launchUrl(this, Uri.parse(url))
     } catch (e: ActivityNotFoundException) {
-        println("${e.message}: ${e.stackTrace}")
+        Timber.e("${e.message}: ${e.stackTrace}")
     }
 }
